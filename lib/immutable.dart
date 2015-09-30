b129cb2ec3c8fe5183d06d8cf2f4f84707ff71ae
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-09-30 14:10:13 +0700

    Add my name and email to the LICENSE file.

--- lib/immutable.dart
+++ lib/immutable.dart
@@ -1,5 +1,6 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2015, Kwang Yul Seo <kwangyul.seo@gmail.com>.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 /// The immutable library.
 ///
